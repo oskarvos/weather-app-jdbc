3412e7bcdb9471971a16dbd754ff3c1f431cc7c3
OLEG <oskaryahoo@gmail.com>
2026-01-16 12:34:39 +0300

    add util DatabaseConnection

---
+++ src/main/java/com/oskarvos/util/DatabaseConnection.java
@@ -0,0 +1,41 @@
+package com.oskarvos.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DatabaseConnection {
+
+    public static Properties properties = new Properties();
+
+    static {
+        try (InputStream input = DatabaseConnection.class
+                .getClassLoader()
+                .getResourceAsStream("database.properties")) {
+
+            if (input == null) {
+                throw new RuntimeException("Unable to find database.properties");
+            }
+
+            properties.load(input);
+
+            Class.forName("org.postgresql.Driver");
+
+        } catch (IOException | ClassNotFoundException e) {
+            throw new RuntimeException("Failed to load database configuration", e);
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
+        String url = properties.getProperty("db.url");
+        String user = properties.getProperty("db.user");
+        String password = properties.getProperty("db.password");
+
+        return DriverManager.getConnection(url, user, password);
+    }
+
+
+}
